be80978db73aa90096af57728d96183ca342477d
jdsuarez97 <152046718+jdsuarez97@users.noreply.github.com>
2024-05-29 17:33:17 -0600

    Cliente.java

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -1,36 +1,119 @@
 package Cliente;
 
-import Modelos.Barco.Barco;
-import Modelos.Mensaje;
-
+import java.net.Socket;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import javax.swing.JOptionPane;
+
+import java.util.Random;
+
+
+import Interfaces.InterfazCliente;
+import Modelos.Mensaje;
+import Modelos.Barco.Barco;
+import Modelos.Tablero.CeldaVacia;
+import Modelos.Tablero.Tablero;
 
 public class Cliente {
-    private String nombre;
-    private final String ip = "localhost";
-    private final int puerto = getPuerto();
+    private final String IP = "localhost";
+    private final int PUERTO = 8089;
+    private Socket socket;
     private DataOutputStream salidaDatos;
-
-
-    private Mensaje mensaje;
+    private InterfazCliente interfazCliente;
+    private ThreadCliente threadCliente;
+    private String nombre;
+    private Barco barco;
     public ObjectOutputStream salida;
-    public Barco barco;
 
-
-    //Métodos
-    public String getNombre() {
-        return nombre;
+    public Cliente(InterfazCliente interfazCliente) {
+        this.interfazCliente = interfazCliente;
+        conectar();
     }
-    public String enviarMensaje(Mensaje mensaje) {
-        System.out.println(mensaje);
-        return "Mensaje enviado: " + mensaje.toString();
+
+    public synchronized void conectar() {
+        try {
+            socket = new Socket(IP, PUERTO);
+            salida = new ObjectOutputStream(socket.getOutputStream());
+            salidaDatos = new DataOutputStream(socket.getOutputStream());
+            threadCliente = new ThreadCliente(socket, this);
+            threadCliente.start();
+            // Pedir el nombre
+            this.nombre = JOptionPane.showInputDialog("Nombre:");
+            salidaDatos.writeUTF(this.nombre);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al conectar con el servidor. Se debería de iniciar el servidor antes de iniciar con el cliente.");
+        }
+    }
+
+    public synchronized void enviarMensaje(Mensaje mensaje) {
+        if (salida != null) {
+            try {
+                salida.writeObject(mensaje);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Tienes que escribir algo para poder enviarlo.");
+        }
+    }
+
+    public void crearBarco(InterfazCliente interfazCliente, Tablero tablero){
+        Random random = new Random();
+        int selectorEstatico = random.nextInt(3);
+        //Caso 0: Inicia con 0 fijo en X
+        //Caso 1: Inicia con 14 fijo en X
+        //Caso 2: Inicia con 0 fijo en Y
+        //Caso 3: Inicia con 14 fijo en Y
+        int posicionBarcoX = 0;
+        int posicionBarcoY = 0;
+
+        switch(selectorEstatico){
+            case 0:
+                posicionBarcoX = 0;
+                posicionBarcoY = random.nextInt(14);
+                break;
+            case 1:
+                posicionBarcoX = 14;
+                posicionBarcoY = random.nextInt(14);
+                break;
+            case 2:
+                posicionBarcoX = random.nextInt(14);
+                posicionBarcoY = 0;
+                break;
+            case 3:
+                posicionBarcoX = random.nextInt(14);
+                posicionBarcoY = 14;
+                break;
+        }
+        barco = new Barco(nombre, posicionBarcoX, posicionBarcoY);
+        tablero.getTableroMapa()[posicionBarcoX][posicionBarcoY] = new CeldaVacia(posicionBarcoX, posicionBarcoY);
+        tablero.getTableroMapa()[posicionBarcoX][posicionBarcoY].setCeldaVisible(true);
+        interfazCliente.mostrarCelda(posicionBarcoX, posicionBarcoY, tablero.getTableroMapa()[posicionBarcoX][posicionBarcoY]);
+        interfazCliente.moverBarcoCelda(posicionBarcoX, posicionBarcoY);
+        interfazCliente.setVida(barco.getNivelSalud());
+        interfazCliente.setOro(barco.getOroDisponible());
+    }
+
+    public void moverBarco(int posicionX, int posicionY){
+        barco.setPosicionX(posicionX);
+        barco.setPosicionY(posicionY);
     }
 
     public Barco getBarco() {
         return barco;
     }
-    private int getPuerto() {
-        return puerto;
+
+    public void setBarco(Barco barco) {
+        this.barco = barco;
+    }
+
+    public InterfazCliente getInterfazCliente() {
+        return interfazCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 }
